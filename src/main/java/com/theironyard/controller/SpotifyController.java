8d68766c7d83d6ee6c065d501fcb5839e052a3f0
jeffreydorney <jjdorney1@gmail.com>
2016-10-11 11:01:16 -0400

    able to gather all songs from all playlists and library - started code to organize into one master list

--- src/main/java/com/theironyard/controller/SpotifyController.java
+++ src/main/java/com/theironyard/controller/SpotifyController.java
@@ -91,52 +91,72 @@ public class SpotifyController {
     }
 
     @RequestMapping(path="/test")
-    public String test(Model model, HttpSession session){
+    public String test(Model model, HttpSession session) throws InterruptedException {
 
-
+        // api session initialization
         Api api = (Api) session.getAttribute("api");
 
+        // list to hold the tracks from library and playlists
+        ArrayList<String> userLibraryTracks = new ArrayList<>();
+        ArrayList<String> userPlaylistTracks;
+
+        // object to hold image data
+        Image image = new Image();
+
+        // object to hold user id
+        String userId;
+
+        // image url for user
+        String imageUrl = "profile_default.jpg";
+
+        // list of image data
+        List<Image> imageData;
+
+        // creates refresh token from api
         userService.refreshToken(api);
 
+        // gets user based on their data and adds to model
         User user = userService.getUser(api);
         model.addAttribute("user", user);
 
-        List<Image> imageData = userService.getUser(api).getImages();
-        Image image = imageData.get(0);
-        String imageUrl = image.getUrl();
+        // sets user id based on the user logged in
+        userId = user.getId();
 
-        ArrayList<String> trackArrayList = userService.getSavedTracks(api);
-
-        model.addAttribute("trackArrayList", trackArrayList);
-
+        // gets the user's image if they have one
+        if(userService.getUser(api).getImages().size() != 0) {
+            // user has an image
+            // sets image to their profile image
+            imageData = userService.getUser(api).getImages();
+            image = imageData.get(0);
+            imageUrl = image.getUrl();
+        }
+        // adds the image data to the model
         model.addAttribute("imageUrl", imageUrl);
         model.addAttribute("image", image);
 
-        //Track track = userService.getUserSavedTracks(api).get(1).getTrack();
+        // gets the user's saved tracks from their library
+        try {
+            // if they have some saved tracks in their library
+            if(api.getMySavedTracks().build().get().getItems().size() != 0) {
 
-        //model.addAttribute("track", track);
+                // calls get tracks method and adds them to the model
+                userLibraryTracks = userService.getSavedTracks(api);
+                model.addAttribute("userLibraryTracks", userLibraryTracks);
+            } else {
 
+                // no saved tracks in library, adds none value to list so it's not empty
+                userLibraryTracks.add("NONE");
+            }
+            // catch exceptions
+        } catch (IOException | WebApiException e) {
+            e.printStackTrace();
+        }
 
-        /*Track track = userService.getSavedTrack(api);
-        model.addAttribute("track", track);*/
+        // gets the user's saved tracks from their playlists
+        userPlaylistTracks = userService.getSavedPlaylists(api, userId);
+        model.addAttribute("userPlaylistTracks", userPlaylistTracks);
 
-        /* Not functioning
-
-        List<LibraryTrack> tracks = userService.getUserSavedTracks(api);
-        model.addAttribute("tracks", tracks);*/
-
-
-
+        // return to page
         return "test";
     }
-
-
-    @RequestMapping(path="/testing")
-    public String test(Model model, String curl){
-
-        model.addAttribute("curl", curl);
-
-        return "testing";
-    }
-
 }
--- src/main/java/com/theironyard/service/UserService.java
+++ src/main/java/com/theironyard/service/UserService.java
@@ -1,7 +1,5 @@
 package com.theironyard.service;
 
-
-import com.theironyard.entity.Playlist;
 import com.theironyard.repository.*;
 import com.wrapper.spotify.Api;
 import com.wrapper.spotify.exceptions.WebApiException;
@@ -41,187 +39,202 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
-    // TODO: method to query the API to get data for the user
-    public User getUserData() {
-        // query
-        // return the collected data
-        return new User();
-    }
 
-    // TODO: method to save the date to the server for accessing
-    public void saveUserData(User user) {
-        //userRepository.save(user);
-    }
+    // setups up the refresh token for the api
+    public void refreshToken(Api api) {
+        try {
+            RefreshAccessTokenCredentials refresh = api.refreshAccessToken().build().get();
+            api.setAccessToken(refresh.getAccessToken());
 
-
-
-    // TODO: method to query API to get list of playlists
-    public ArrayList<Playlist> getListUserPlaylists() {
-        // query
-        // assign collected data to List<Playlist>
-        return new ArrayList<>();
-    }
-//
-//    // TODO: method to parse playlists for tracks
-//    public ArrayList<Track> getListUserPlaylistTracks(ArrayList<Playlist> playlists) {
-//        // parse the playlists one by one
-//        for (Playlist playlist : playlists) {
-//            saveUserPlaylistTracks();
-
-        //}
-//        // parse list for songs
-//        ArrayList<Track> tracks = new ArrayList<>();
-//        return tracks;
-//    }
-
-    // TODO: method to iterate over List<Song> to save or something
-    public void saveUserPlaylistSongs(ArrayList<Track> tracks) {
-        for (Track track : tracks) {
-            // ????? userRepository.save(song);
+        } catch (IOException | WebApiException e) {
+            e.printStackTrace();
         }
     }
 
-    // TODO: method to query the API to get data for the friend(s)
-    public User getFriendData() {
-        // query
-        // return the collected data
-        return new User();
-    }
 
-    //
-    public void refreshToken (Api api){
-            try {
-                RefreshAccessTokenCredentials refresh = api.refreshAccessToken().build().get();
-                api.setAccessToken(refresh.getAccessToken());
-
-            } catch (IOException | WebApiException e) {
-                e.printStackTrace();
-            }
-        }
-
-
+    // gets the user from the api
     public User getUser(Api api) {
         User user = null;
 
         try {
+            // gets the user's data
             user = api.getMe().build().get();
         } catch (IOException | WebApiException e) {
             e.printStackTrace();
         }
 
+        // returns the user
         return user;
     }
 
 
-    public ArrayList<String> getSavedTracks(Api api) throws NullPointerException {
+    // method to gather the user's saved tracks and return their ID #'s in an arraylist of strings
+    public ArrayList<String> getSavedTracks(Api api) throws NullPointerException, InterruptedException {
+
+        // page of library tracks
         Page<LibraryTrack> libraryTrackPage;
+
+        // size of the user's library
+        int librarySize;
+
+        // track object to hold the track
         Track track;
+
+        // arraylist of tracks to return
         ArrayList<String> tracks = new ArrayList<>();
 
         try {
             // offset initializer
             int offset = 0;
 
-            // DO: get the tracks -- WHILE: next track is not null
-            // do {
+            // DO: go gather the tracks from the user
+            do {
+
                 // gets a Page of LibraryTracks 50 long at an ever increasing offset
                 libraryTrackPage = api.getMySavedTracks()
                         .limit(50)
                         .offset(offset)
                         .build().get();
 
+                librarySize = libraryTrackPage.getTotal();
+
                 // for loop to add each track's ID to an ArrayList of Track
-                for (int x = 1; x < 50; x++) {
+                for (int x = 1; x < librarySize; x++) {
                     track = libraryTrackPage.getItems().get(x).getTrack();
                     tracks.add(track.getId());
                 }
                 // increment offset
                 offset++;
 
-            // } while (libraryTrackPage.getNext() != null);
+                // delay between searches (in milliseconds) - to keep spotify from booting us out of process
+                Thread.sleep(10);
 
+                // WHILE: next page of tracks is not null
+            } while (libraryTrackPage.getNext() != null);
+
+            // catch exceptions
         } catch (IOException | WebApiException e) {
             e.printStackTrace();
         }
 
+        // returns the list of track ids
         return tracks;
     }
 
-    // collect albums
-    public ArrayList<String> getSavedAlbums(Api api) throws NullPointerException {
-        Page<LibraryTrack> libraryTrackPage;
-        Track track;
+
+    // gets a list of playlists from the user, and then ultimately returns all the tracks
+    public ArrayList<String> getSavedPlaylists(Api api, String userId) {
+
+        // sets default playlists amount to zero
+        Page<SimplePlaylist> pageTotalPlaylists = new Page<>();
+
+        // playlist total count
+        int totalPlaylists = 0;
+
+        // list of playlists from the userId
+        List<SimplePlaylist> playlists;
+
+        // track id object
+        String trackId;
+
+        // list of track ids from the playlist
+        ArrayList<String> playlistTrackIds;
+
+        // list of tracks to add
+        ArrayList<String> tracksToAdd = new ArrayList<>();
+
+        // list of tracks to return
         ArrayList<String> tracks = new ArrayList<>();
 
+        // collects page of total playlists from api
         try {
-            // offset initializer
-            int offset = 0;
+            pageTotalPlaylists = api.getPlaylistsForUser(userId).build().get();
+        } catch (IOException | WebApiException e) {
+            e.printStackTrace();
+        }
 
-            // DO: get the tracks -- WHILE: next track is not null
-            // do {
-            // gets a Page of LibraryTracks 50 long at an ever increasing offset
-            libraryTrackPage = api.getMySavedTracks()
-                    .limit(50)
-                    .offset(offset)
-                    .build().get();
+        // counts the total playlists
+        totalPlaylists = pageTotalPlaylists.getTotal();
 
-            // for loop to add each track's ID to an ArrayList of Track
-            for (int x = 1; x < 50; x++) {
-                track = libraryTrackPage.getItems().get(x).getTrack();
-                tracks.add(track.getId());
+        // checks if the user has playlists
+        if (totalPlaylists != 0) {
+
+            // gathers all the user's playlists
+            playlists = pageTotalPlaylists.getItems();
+
+            // for each to iterate over the playlists and get uid and playlist id
+            for (SimplePlaylist playlist : playlists) {
+
+                // gets the playlist id from the playlist
+                String playlistId = playlist.getId();
+
+                // gets the user id from the playlist
+                String uid = playlist.getOwner().getId();
+
+                // sets the tracks to the searched playlist
+                tracksToAdd = getPlaylistTracks(api, uid, playlistId);
+
+                for (int x = 0; x < tracksToAdd.size(); x++) {
+                    trackId = tracksToAdd.get(x);
+                    tracks.add(tracks.size(), trackId);
+                }
             }
-            // increment offset
-            offset++;
+        }
+        // return the collected tracks from the playlists
+        return tracks;
+    }
 
-            // } while (libraryTrackPage.getNext() != null);
 
+    // get a list of tracks from the api for a given user and playlistid
+    public ArrayList<String> getPlaylistTracks(Api api, String userId, String playlistId) {
+        // creates object to hold the track ids
+        ArrayList<String> tracks = new ArrayList<>();
+
+        Page<PlaylistTrack> playlistTracksPage;
+        List<PlaylistTrack> playlistTracks;
+
+        try {
+            // created playlisttrack list to iterate over
+            playlistTracksPage = api.getPlaylistTracks(userId, playlistId).build().get();
+
+            if (playlistTracksPage.getTotal() == 0) {
+                playlistTracksPage = api.getPlaylistTracks("spotify", playlistId).build().get();
+            }
+
+            playlistTracks = playlistTracksPage.getItems();
+            int playlistSize = playlistTracks.size();
+
+            // iterates over the list to pull out the track id
+            for (int x = 0; x < playlistSize; x++) {
+                String trackId = playlistTracks.get(x).getTrack().getId();
+                playlistTracks.get(x).getTrack().getUri();
+                tracks.add(x, trackId);
+            }
+            // exception catches
         } catch (IOException | WebApiException e) {
             e.printStackTrace();
         }
+        // returns the list of track ids
+        return tracks;
+    }
+
+
+    // building this out -- work in progress
+    public ArrayList<String> getFinalNewMusicList(Api api, String userId, ArrayList<String> userSavedTracks, ArrayList<String> userPlaylistTracks) {
+        ArrayList<String> tracks = new ArrayList<>();
+
+        int playlistTrackCounter = userPlaylistTracks.size();
+        String playlistTrackToCheck;
+
+        for (int x = 0; x < playlistTrackCounter; x++) {
+            playlistTrackToCheck = userPlaylistTracks.get(x);
+
+
+        }
+
 
         return tracks;
     }
 
-    public Track getSavedTrack(Api api) {
-        Track track = null;
-
-        try {
-            track = api.getTrack("6QttDorIxNUehXGiqtiq1P").build().get();
-        } catch (IOException | WebApiException e) {
-            e.printStackTrace();
-        }
-
-        return track;
-    }
-
-    public List<LibraryTrack> getUserSavedTracks(Api api) {
-        List<LibraryTrack> tracks = null;
-
-        try {
-            tracks = api.getMySavedTracks().build().get().getItems();
-
-            //api.getMySavedTracks().build().get().getItems().get(x).getTrack()
-
-        } catch (WebApiException | IOException e) {
-            e.printStackTrace();
-        }
-
-        return tracks;
-    }
-
-    public String getTracksCurl() {
-        String country = "US";
-        Integer offset = 0;
-        String token;
-        return country;
-    }
-
-    public String getTracksCurl(String country, Integer offset, String token) {
-
-        return "curl -X GET \"https://api.spotify.com/v1/me/tracks?market=" +
-                country + "&limit=50&offset=" + offset +
-                "\" -H \"Accept: application/json\" -H \"Authorization: Bearer " +
-                token + "\"";
-    }
 
 }
